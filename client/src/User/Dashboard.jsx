ac936d2558141c0cca0a5015b1b631bd64bec0e8
Yash Pandey <141578029+yash264@users.noreply.github.com>
2024-09-15 14:42:06 +0530

    Update Dashboard.jsx

--- client/src/User/Dashboard.jsx
+++ client/src/User/Dashboard.jsx
@@ -22,11 +22,10 @@ function Dashboard(){
     }
 
     const [values,setValues] = useState([])   
-    //axios.defaults.withCredentials = true;
+    axios.defaults.withCredentials = true;
     useEffect(()=>{
         axios.get('https://rozgaar-server.vercel.app/dashboard')
         .then(values => {
-            console.log(values);
             setValues(values.data.userData)
         })
         .catch(error => console.log(error))
