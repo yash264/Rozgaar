866db06263c057f03d5fe6c6a965a40c0cb131f4
Yash Pandey <141578029+yash264@users.noreply.github.com>
2024-09-27 23:34:20 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -26,15 +26,6 @@ const corsOptions ={
 
 app.use(cors(corsOptions));
 
-/*app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})*/
-
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
